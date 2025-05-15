1eb134ffdcf4827299a7326df8d3745e8b887921
yuuki9089 <yuukiou90@gmail.com>
2025-05-16 01:53:24 +0900

    Add: スタッフ情報更新画面

---
+++ ChangeStaffInfo.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace KSM
+{
+    partial class ChangeStaffInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayId = new System.Windows.Forms.Label();
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.splitPanel = new System.Windows.Forms.Panel();
+            this.UserIDLabel = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.headerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // displayId
+            // 
+            this.displayId.AutoSize = true;
+            this.displayId.Font = new System.Drawing.Font("HGP創英角ﾎﾟｯﾌﾟ体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.displayId.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(95)))), ((int)(((byte)(154)))));
+            this.displayId.Location = new System.Drawing.Point(125, 18);
+            this.displayId.Name = "displayId";
+            this.displayId.Size = new System.Drawing.Size(251, 33);
+            this.displayId.TabIndex = 3;
+            this.displayId.Text = "スタッフ情報更新";
+            this.displayId.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.White;
+            this.headerPanel.Controls.Add(this.displayId);
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(501, 64);
+            this.headerPanel.TabIndex = 4;
+            this.headerPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.headerPanel_Paint);
+            // 
+            // splitPanel
+            // 
+            this.splitPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(154)))), ((int)(((byte)(214)))));
+            this.splitPanel.Location = new System.Drawing.Point(0, 64);
+            this.splitPanel.Name = "splitPanel";
+            this.splitPanel.Size = new System.Drawing.Size(501, 10);
+            this.splitPanel.TabIndex = 6;
+            // 
+            // UserIDLabel
+            // 
+            this.UserIDLabel.Location = new System.Drawing.Point(219, 190);
+            this.UserIDLabel.Multiline = true;
+            this.UserIDLabel.Name = "UserIDLabel";
+            this.UserIDLabel.Size = new System.Drawing.Size(190, 33);
+            this.UserIDLabel.TabIndex = 28;
+            this.UserIDLabel.TextChanged += new System.EventHandler(this.UserIDLabel_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(219, 246);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 32);
+            this.comboBox1.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(82, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 38);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "性別";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(82, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 38);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "スタッフ名";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(179)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("游ゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(387, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 37);
+            this.button2.TabIndex = 37;
+            this.button2.Text = "削除";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("游ゴシック", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(182, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 55);
+            this.button1.TabIndex = 38;
+            this.button1.Text = "更新";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(82, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 38);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "権限";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(219, 304);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(190, 32);
+            this.comboBox2.TabIndex = 40;
+            // 
+            // ChangeStaffInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(501, 554);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.UserIDLabel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.splitPanel);
+            this.Controls.Add(this.headerPanel);
+            this.Name = "ChangeStaffInfo";
+            this.Text = "スタッフ情報更新";
+            this.Load += new System.EventHandler(this.ChangeStudentInfo_Load);
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label displayId;
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.Panel splitPanel;
+        private System.Windows.Forms.TextBox UserIDLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ ChangeStaffInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSM
+{
+    public partial class ChangeStaffInfo : Form
+    {
+        public ChangeStaffInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void headerPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserIDLabel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChangeStudentInfo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
