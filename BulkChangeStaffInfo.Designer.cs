fbcd27b02b4728d52cbaeb8a11f4c2d94c5ac533
yuuki9089 <yuukiou90@gmail.com>
2025-05-17 02:25:39 +0900

    Add: スタッフ情報一括更新画面

---
+++ BulkChangeStaffInfo.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace KSM
+{
+    partial class BulkChangeStaffInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contentPanel = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.UserIDLabel = new System.Windows.Forms.TextBox();
+            this.contentPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(96)))), ((int)(((byte)(130)))));
+            this.panel1.Location = new System.Drawing.Point(96, 88);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1058, 5);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("游ゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(1043, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "参照";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(92, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 50);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "アップロードファイル";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // contentPanel
+            // 
+            this.contentPanel.BackColor = System.Drawing.Color.AliceBlue;
+            this.contentPanel.Controls.Add(this.button2);
+            this.contentPanel.Controls.Add(this.dataGridView1);
+            this.contentPanel.Controls.Add(this.UserIDLabel);
+            this.contentPanel.Controls.Add(this.label1);
+            this.contentPanel.Controls.Add(this.button1);
+            this.contentPanel.Controls.Add(this.panel1);
+            this.contentPanel.ForeColor = System.Drawing.Color.Black;
+            this.contentPanel.Location = new System.Drawing.Point(0, 80);
+            this.contentPanel.Name = "contentPanel";
+            this.contentPanel.Size = new System.Drawing.Size(1264, 689);
+            this.contentPanel.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("游ゴシック", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(538, 595);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 63);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "取込";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(127, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 448);
+            this.dataGridView1.TabIndex = 26;
+            // 
+            // UserIDLabel
+            // 
+            this.UserIDLabel.Location = new System.Drawing.Point(331, 35);
+            this.UserIDLabel.Multiline = true;
+            this.UserIDLabel.Name = "UserIDLabel";
+            this.UserIDLabel.Size = new System.Drawing.Size(685, 33);
+            this.UserIDLabel.TabIndex = 25;
+            // 
+            // BulkChangeStaffInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 765);
+            this.Controls.Add(this.contentPanel);
+            this.Name = "BulkChangeStaffInfo";
+            this.Text = "スタッフ情報一括更新";
+            this.Resize += new System.EventHandler(this.ScoreEntry_Resize);
+            this.Controls.SetChildIndex(this.contentPanel, 0);
+            this.contentPanel.ResumeLayout(false);
+            this.contentPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel contentPanel;
+        private System.Windows.Forms.TextBox UserIDLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
