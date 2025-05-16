93965d5fc2f5a6803c65eac2ee132b5d78c1cb89
yuuki9089 <yuukiou90@gmail.com>
2025-05-17 02:26:29 +0900

    Add: データ出力画面

---
+++ DataOutput.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSM
+{
+    public partial class DataOutput : Form
+    {
+        private Form mainForm;
+        public DataOutput(Form mainForm)
+        {
+            InitializeComponent();
+            this.mainForm = mainForm;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserIDLabel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
